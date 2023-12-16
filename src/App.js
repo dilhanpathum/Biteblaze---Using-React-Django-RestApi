2527fbcb39e95cdbd622837df0d11f615bdc9bef
kishobigan <karunairasa.kishobigan@gmail.com>
2023-12-16 09:48:48 +0530

    Order History Created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Admin from "./pages/Admin/Admin";
+import OrderHistory from "./pages/OrderHistory/OrderHistory";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="Admin" element={<Admin />} />
+        <Route path="/orderHistory" element={<OrderHistory/>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/OrderHistory/Section.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
+import "../../styles/orderHistory.css"
+
+function Section() {
+    return (
+        <section className='order_history_section'>
+            <Container>
+                <Row>
+                    <Col className="mt-5">
+                        <div className="mt-5 mb-lg-0">
+                            <h2 className="order_history_title">order History</h2>
+                        </div>
+                        <div>
+                            <table className="order_history_table">
+                                <thead>
+                                    <tr>
+                                        <td>Date</td>
+                                        <td>Address</td>
+                                        <td>Contact</td>
+                                        <td>Price</td>
+                                        <td>Delivery Status</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>16 Dec 2023</td>
+                                        <td>No 17, main street, Badulla</td>
+                                        <td>0711111111</td>
+                                        <td>Rs. 3825</td>
+                                        <td>Pending</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Section
