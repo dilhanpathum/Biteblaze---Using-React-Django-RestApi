d29ed56f0ead302b0f112c24fe9f87de65fc27cf
Bodhika-Git <105534658+Bodhika-Git@users.noreply.github.com>
2023-12-27 23:08:25 -0800

    Add Foodform

---
+++ src/pages/Foodform/Foodform.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Section4 from "./Section4";
+
+const FoodForm = () => {
+    return (
+       
+            <Section4/>
+    
+    );
+}
+
+export default FoodForm;
